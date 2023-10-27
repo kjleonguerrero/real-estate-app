2194f83e998e4877cbf7152a498768c3a22b4de2
kjleonguerrero <139664760+kjleonguerrero@users.noreply.github.com>
2023-10-28 06:34:19 +1000

    created create controller and test. Test still fails

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
   get "/home" => "homes#index"
   get "/home" => "homes#show"
+  post "/home" => "homes#create"
 end
--- test/controllers/homes_controller_test.rb
+++ test/controllers/homes_controller_test.rb
@@ -10,10 +10,16 @@ class HomesControllerTest < ActionDispatch::IntegrationTest
   end
 
   test "show" do
-    get "/homes/#{Photo.first.id}.json"
+    get "/home/#{Photo.first.id}.json"
     assert_response 200
 
     data = JSON.parse(response.body)
     assert_equal ["description", "year_built", "square_feet", "bedrooms", "bathrooms", "floors", "availability", "address", "price"], data.keys
+  end
+
+  test "create" do
+    assert_difference "Product.count", 1 do
+      post "/products.json", params: { description: "test product", year_built: 1, square_feet: 12312, bedrooms: 1, bathrooms: 1, floors: 1, availability: true, address: "Test address", price: 1232122 }
+    end
   end
 end
